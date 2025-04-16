cbe422566cd7d077101b5085b22587f78a5fa6f1
Draksham Tharun <your-email@example.com>
2025-04-17 00:21:44 +0530

    Testing

---
+++ Demo-App/src/main/java/com/Tharun/Demo/App/Hello.java
@@ -0,0 +1,12 @@
+package com.Tharun.Demo.App;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("/Hello")
+    public String greet(){
+        return "Hello world, My nane is Tharun i am learning spring boot" ;
+    }
+}
---
+++ Demo-App/src/test/java/com/Tharun/Demo/App/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tharun.Demo.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
